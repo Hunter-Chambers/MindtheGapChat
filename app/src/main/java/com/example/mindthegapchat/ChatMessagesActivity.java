02536ba51fde649387b8ee3532d00a60ff742d52
hunter-chambers <hrchambers.333@gmail.com>
2021-03-25 22:54:06 -0500

    Messages show up in chat window

--- app/src/main/java/com/example/mindthegapchat/ChatMessagesActivity.java
+++ app/src/main/java/com/example/mindthegapchat/ChatMessagesActivity.java
@@ -2,13 +2,38 @@ package com.example.mindthegapchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class ChatMessagesActivity extends AppCompatActivity {
+
+    //ScrollView
+
+    LinearLayout server_messages;
+    EditText message_to_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_messages);
+
+        server_messages = findViewById(R.id.server_messages);
+
+        message_to_send = findViewById(R.id.message_field);
+    }
+
+    public void sendMessage(View view) {
+        TextView message = new TextView(this);
+        message.setBackgroundColor(Color.argb(255, 217, 217, 217));
+        message.setText(message_to_send.getText().toString());
+        message.setTextColor(Color.BLACK);
+        message.setTextSize(25);
+
+        server_messages.addView(message);
     }
 }
