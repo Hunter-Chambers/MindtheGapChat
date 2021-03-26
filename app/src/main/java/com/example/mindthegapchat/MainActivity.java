79e3130655ae64a74a73c0ae0f45cc05367560ff
Thaumcaster <nathanbradendayton@gmail.com>
2021-03-25 21:17:02 -0500

    Button Added
    Button Added

--- app/src/main/java/com/example/mindthegapchat/MainActivity.java
+++ app/src/main/java/com/example/mindthegapchat/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.mindthegapchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.submitButton);
+    }
+
+    public void tableActivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), ChatMessagesActivity.class);
+        startActivity(intent);
     }
 }
