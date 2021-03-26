8d62b8cf9c36eb0c253c7a73138b6e283b5047d4
hunter-chambers <hrchambers.333@gmail.com>
2021-03-26 00:17:40 -0500

    Floating messages to the side

--- app/src/main/java/com/example/mindthegapchat/ChatMessagesActivity.java
+++ app/src/main/java/com/example/mindthegapchat/ChatMessagesActivity.java
@@ -2,8 +2,11 @@ package com.example.mindthegapchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -13,20 +16,31 @@ import android.widget.TextView;
 
 public class ChatMessagesActivity extends AppCompatActivity {
 
+    Intent intent;
+
     ScrollView scrollview;
 
     LinearLayout server_messages;
     EditText message_to_send;
+
+    String username, ipv4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_messages);
 
+        intent = getIntent();
+
         scrollview = findViewById(R.id.messages_scrollview);
 
         server_messages = findViewById(R.id.server_messages);
         message_to_send = findViewById(R.id.message_field);
+
+        username = intent.getStringExtra("USERNAME");
+        ipv4 = intent.getStringExtra("IPv4");
+        Log.i("HERE", username);
+        Log.i("HERE", ipv4);
     }
 
     public void sendMessage(View view) {
@@ -36,12 +50,19 @@ public class ChatMessagesActivity extends AppCompatActivity {
         message.setTextColor(Color.BLACK);
         message.setTextSize(25);
 
-        server_messages.addView(message);
-        scrollview.post(new Runnable() {
-            @Override
-            public void run() {
-                scrollview.fullScroll(View.FOCUS_DOWN);
-            }
-        });
+        LinearLayout container = new LinearLayout(this);
+        container.setOrientation(LinearLayout.HORIZONTAL);
+        container.setWeightSum(1);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 0, 0, 20);
+        container.setLayoutParams(params);
+
+        message.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 0.5f));
+        message.setGravity(Gravity.END);
+        container.addView(message);
+        container.setGravity(Gravity.END);
+
+        server_messages.addView(container);
+        scrollview.post(() -> scrollview.fullScroll(View.FOCUS_DOWN));
     }
 }
